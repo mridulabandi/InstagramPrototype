d31f36b61d783ab3f133016327c7d6d522fb7d41
mridula.bandi <mridula.bandi@infosys.com>
2020-01-20 11:18:42 +0530

    Initial Commit

---
+++ InstagramDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstagramDemo
+//
+//  Created by mysmac_adm!n on 20/01/20.
+//  Copyright © 2020 Mridula. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
